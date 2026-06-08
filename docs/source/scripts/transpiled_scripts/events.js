06a4c447535076a1ba52647086b84b47d1615237
Kira Ash Stephenson <SolaasAshenOnslaught@proton.me>
2026-06-08 15:49:36 -0700

    Added Alt Text + Fixed Jump Bug
    Almost forgot to add alternative text to the images, but now they have it.  Also, the weird id-jump bug where it wasn't jumping to the top of an element all the time has been fixed.  It seemed to be an issue with the browser taking over the scrolling, so I forced the scroll to the top of a page after loading and then the highlight scroll takes place.

--- docs/source/scripts/transpiled_scripts/events.js
+++ docs/source/scripts/transpiled_scripts/events.js
@@ -5,14 +5,15 @@ let HEAVENLY_EARTH = new SM.Style(SD.STYLE_HEAVENLY_EARTH);
 document.addEventListener("DOMContentLoaded", () => {
     HEAVENLY_EARTH.apply();
 });
-// Listening to see if a highlight is necessary upon loading the page and if an id-jump was made.
-window.addEventListener("load", highlight_target);
-window.addEventListener("hashchange", highlight_target);
+// Listening to see if a highlight is necessary upon loading the page; works for id-jumps and refreshes.
+window.addEventListener("load", () => {
+    // I'm forcing the scroll to the top first to ensure that the highlight_target scroll doesn't get hooked by a strange load.
+    window.scrollTo({ top: 0, behavior: "instant" });
+    highlight_target();
+});
 // Mainly used for highlighting sources after an inline source tag has been clicked, but it will also
 // highlight anything that gets clicked for a hash jump (id jumping).  So if you start seeing rectangles
 // around elements you're trying to id-jump to and you don't like that, then know that this is the culprit.
-//
-// Will not work if the target has no parent container.
 //
 // This function is paired with 2 window event listeners inside events.ts.
 function highlight_target() {
@@ -34,7 +35,8 @@ function highlight_target() {
             target.style.transition = "box-shadow 1s ease";
             // Scrolls the target into view.  The math is to prevent the navigation bar from blocking the target's view.
             const nav = document.querySelector("nav");
-            const target_position = target.parentElement.offsetTop - nav.offsetHeight;
+            const nav_height = nav ? nav.offsetHeight : 0;
+            const target_position = target.offsetTop - nav_height - 50;
             window.scrollTo({ top: target_position, behavior: "smooth" });
         }
     }
